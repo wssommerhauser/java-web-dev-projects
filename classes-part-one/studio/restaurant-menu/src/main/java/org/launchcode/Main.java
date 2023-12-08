abe020e43a75af3fb8971b7eb0e283686567640a
William Sommerhauser <wssommerhauser@gmail.com>
2023-12-07 20:19:25 -0600

    classes initialized

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,47 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
+}
+class Menu {
+    private ArrayList<MenuItem> itemList = new ArrayList<>();
+    private Date dateModified = new Date();
+
+    public void lastModified(){
+        //returns how many days since dateModified was last changed.
+        return;
+    }
+}
+class MenuItem {
+    private String itemName;
+    private String itemDescription;
+    private Double price;
+    private ArrayList<String> category = new ArrayList<>();
+    //private Date dateAdded = new Date();
+    private Boolean isNew;
+    public MenuItem(){
+
+    }
+    public MenuItem(String name, String description, Double price, ArrayList<String> category, Boolean isNew){
+        this.itemName = name;
+        this.itemDescription = description;
+        this.price = price;
+        this.category = category;
+        this.isNew = isNew;
+
+    }
+//    public Boolean isNew(){
+//        //returns true is dateAdded is within a span of the current date
+//
+//        /*if(dateAdded.){
+//            return true;
+//        }*/
+//        return false;
+//    }
 }
